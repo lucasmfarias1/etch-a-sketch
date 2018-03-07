f5597cd761545b743136c127d946f13e370d4905
Lucas Manzano Farias <lucasmfarias1@gmail.com>
2018-03-07 19:10:03 -0300

    90% of the work done, random color function added, reset button with a slider for size selection, page styling and positioning

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,35 @@
 const mainContainer = document.querySelector('main');
+const gridSlider = document.querySelector('#grid-slider');
+const sizeDisplay = document.querySelector('#size-display');
+const resizeButton = document.querySelector('#resize-button');
 
-createGrid(16);
+createGrid();
 
-function createGrid (size) {
+mainContainer.addEventListener('mouseover', function(event) {
+  if (event.target.style.backgroundColor == '') {
+    event.target.style.backgroundColor = getRandomColor();
+  }
+});
+
+gridSlider.addEventListener('change', function() {
+  sizeDisplay.innerHTML= ` ${this.value}x${this.value}`;
+});
+sizeDisplay.innerHTML= ` ${gridSlider.value}x${gridSlider.value}`;
+
+resizeButton.onclick = createGrid;
+
+function createGrid () {
+  const size = gridSlider.value;
+  mainContainer.innerHTML = '';
+  mainContainer.setAttribute("style",
+      `grid-template-columns: repeat(${size}, 1fr);`);
+
   for (var i = 0; i < (size * size); i++) {
     mainContainer.appendChild(document.createElement('div'));
   }
+}
+
+function getRandomColor () {
+  const hueValue = Math.floor(Math.random() * 360);
+  return `hsl(${hueValue}, 50%, 51%)`;
 }
